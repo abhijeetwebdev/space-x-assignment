0f1eea1ed31775169500168403de2d71fdb027bc
Abhijeet Vilas <Abhijeet.Vilas@ibm.com>
2020-09-05 19:54:08 +0530

    add: property bindings

--- src/app/flight/list/list.component.ts
+++ src/app/flight/list/list.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Flight } from '../../models/flight.model';
 
 @Component({
   selector: 'app-list',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list.component.css']
 })
 export class ListComponent implements OnInit {
+
+  @Input() flights: Flight[];
 
   constructor() { }
 
