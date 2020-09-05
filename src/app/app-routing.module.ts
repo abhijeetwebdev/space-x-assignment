9d6cdbf3ff3cfa87a05999f0816b0e2a9990a89c
Abhijeet Vilas <Abhijeet.Vilas@ibm.com>
2020-09-05 19:27:29 +0530

    wip: UI

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // {
+  //   path: '',
+  //   pathMatch: 'full',
+  //   redirectTo: 'flight'
+  // },
+  {
+    path: 'flight',
+    loadChildren: () => import('./flight/flight.module').then(m => m.FlightModule) // Lazy loading flight module
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
