20b5a1e26206c5bc50545c025776f6cccc9aa211
Abhijeet Salunkhe <abhijeetwebdev@gmail.com>
2023-01-20 11:52:06 +0530

    wip: filters

--- src/app/flight/flight.component.ts
+++ src/app/flight/flight.component.ts
@@ -12,6 +12,7 @@ import { Flight } from '../models/flight.model';
 export class FlightComponent implements OnInit {
 
   flights$: Observable<Flight>;
+  params = { limit: 10 };
 
   constructor(private apiService: ApiService) { }
 
@@ -19,13 +20,16 @@ export class FlightComponent implements OnInit {
     this.flights$ = this.getFlights();
   }
 
-  getFlights(): Observable<any> {
-    const params = { limit: 10 };
+  getFlights(args?: any): Observable<any> {
+    const params = args ? args : this.params;
     return this.apiService.getWithParams('/v3/launches', params);
   }
 
   updateFilter(data) {
     // update query and search again
+    const params = {...this.params };
+    params[data.key] = data.value;
+    this.getFlights(params);
   }
 
 }
