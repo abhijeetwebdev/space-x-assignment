ea71845952e1bc4cb11aa5dd621a299b5f6ddbbf
Abhijeet Salunkhe <abhijeetwebdev@gmail.com>
2023-01-20 11:52:04 +0530

    wip: UI

--- src/app/flight/flight.module.ts
+++ src/app/flight/flight.module.ts
@@ -4,13 +4,23 @@ import { FlightComponent } from './flight.component';
 import { FilterComponent } from './filter/filter.component';
 import { CardComponent } from './card/card.component';
 import { ListComponent } from './list/list.component';
+import { Routes, RouterModule } from '@angular/router';
 
-
+const routes: Routes = [
+  {
+    path: '',
+    component: FlightComponent
+  }
+];
 
 @NgModule({
   declarations: [FlightComponent, FilterComponent, CardComponent, ListComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(routes)
+  ],
+  exports: [
+    RouterModule
   ]
 })
 export class FlightModule { }
